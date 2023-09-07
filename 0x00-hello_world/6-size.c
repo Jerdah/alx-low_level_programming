ba485d11b6859ceffbd86437aa5699983a275f7e
Jerdah <idjerop@gmail.com>
2023-09-07 18:11:55 +0000

    5. Hello, printf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the size of various computer types
+ *
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
