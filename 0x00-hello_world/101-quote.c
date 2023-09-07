1d345caff6f66fc8368fe530a42ffbb995af3d9b
Jerdah <idjerop@gmail.com>
2023-09-07 20:33:49 +0000

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,12 @@
  *
  * Description: A C program that prints exact message to standard error.
  *
- * Return: Always 1 (Error)
+ * Return: Always 1 (Standard Error)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"	stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			stdout);
 	return (1);
 }
