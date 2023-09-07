d4b66737cc554702d42752fa70e8eec983293b23
Jerdah <idjerop@gmail.com>
2023-09-07 18:42:11 +0000

    correction of string output

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-	printf("Char size: %zu byte(s)\n", sizeof(char));
-	printf("Int size: %zu byte(s)\n", sizeof(int));
-	printf("Long int size: %zu byte(s)\n", sizeof(long int));
-	printf("Long long int size: %zu byte(s)\n", sizeof(long long int));
-	printf("Float size: %zu byte(s)\n", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
