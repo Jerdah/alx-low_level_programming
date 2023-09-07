665db59b3a5d936a9c942b08aa9d79097a40b713
Jerdah <idjerop@gmail.com>
2023-09-07 19:18:25 +0000

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the specified message to standard error.
+ *
+ * Return: Always 1 (Standard Error)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
