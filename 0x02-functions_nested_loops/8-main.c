a9bbfb84882f6bdf1490aac6fe2ada0cff5ed437
Jerdah <idjerop@gmail.com>
2023-09-12 21:53:31 +0000

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description:  tests the jack_bauer function.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	jack_bauer();
+
+	return (0);
+}
