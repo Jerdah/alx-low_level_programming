235c54bed07520c28b2edaef6e30b538c01c5c66
Jerdah <idjerop@gmail.com>
2023-09-12 22:03:33 +0000

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: tests the times_table function.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	times_table();
+	return (0);
+}
