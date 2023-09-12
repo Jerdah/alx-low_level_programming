a0759936628e06d2820aa2e8ecf686a992c314a4
Jerdah <idjerop@gmail.com>
2023-09-12 22:59:30 +0000

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - sums up two integers
+ * 
+ * @a: this is the first term
+ * @b: this is the second term
+ *  
+ * Return: output of addition of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	print_number(n);
+	_putchar('\n');
+	
+	return (0);
+}
+
