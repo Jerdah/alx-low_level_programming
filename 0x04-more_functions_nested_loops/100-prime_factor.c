e5993e396d888187de6692dbb867d1b6e2f8c1cb
Jerdah <idjerop@gmail.com>
2023-09-14 19:23:37 +0000

    a program that finds and prints the largest prime factor of the number 612852475143

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,15 +3,15 @@
 /**
  * is_prime - Check if an integer is prime.
  * @n: The integer to check.
+ *
  * Return: 1 if n is prime, otherwise 0.
  */
-
 int is_prime(long n)
 {
+	long i;
+
 	if (n <= 1)
 		return (0);
-
-	long i;
 
 	for (i = 2; i * i <= n; ++i)
 	{
@@ -25,9 +25,9 @@ int is_prime(long n)
 /**
  * largest_prime_factor - Find the largest prime factor of an integer.
  * @n: The integer to factor.
+ *
  * Return: The largest prime factor of n.
  */
-
 long largest_prime_factor(long n)
 {
 	long i = 2;
@@ -44,9 +44,9 @@ long largest_prime_factor(long n)
 
 /**
  * main - Find and print the largest prime factor of 612852475143.
+ *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	printf("The largest prime factor of 612852475143 is %ld\n",
