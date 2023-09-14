2bcc8140c51cbd1d6d373daf1a5bc30ba88c7c29
Jerdah <idjerop@gmail.com>
2023-09-14 11:18:05 +0000

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,16 +4,16 @@
 * main - Entry point
 *
 * Description: checks function that prints if integer is positive or negative
-* 
+*
 * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 0;
-        positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
