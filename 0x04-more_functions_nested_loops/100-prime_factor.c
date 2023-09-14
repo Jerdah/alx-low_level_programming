de91b432384208f3673efd0e29a9a6116efab4e6
Jerdah <idjerop@gmail.com>
2023-09-14 19:40:16 +0000

    a program that finds and prints the largest prime factor of the number 612852475143

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,55 +1,53 @@
 #include <stdio.h>
 
 /**
- * is_prime - Check if an integer is prime.
- * @n: The integer to check.
+ * prime_no - check if integer is a prime number
+ * @n: integer to check
  *
  * Return: 1 if n is prime, otherwise 0.
  */
-int is_prime(long n)
+
+int prime_no(long n)
 {
 	long i;
+	long i_max = n / 2;
 
-	if (n <= 1)
-		return (0);
-
-	for (i = 2; i * i <= n; ++i)
+	for (i = 2; i < i_max; ++i)
 	{
 		if (n % i == 0)
 			return (0);
 	}
-
 	return (1);
 }
 
 /**
- * largest_prime_factor - Find the largest prime factor of an integer.
- * @n: The integer to factor.
+ * largest_prime_factor - find the largest prime_factor of an integer.
+ * @n: integer to factor
  *
- * Return: The largest prime factor of n.
+ * Return: largest prime factor of n.
  */
+
 long largest_prime_factor(long n)
 {
 	long i = 2;
 
-	while (!is_prime(n))
+	while (!prime_no(n))
 	{
-		while (n % i || !is_prime(i))
+		while (n % i || !prime_no(i))
 			++i;
 		n /= i;
 	}
-
 	return (n);
 }
 
 /**
- * main - Find and print the largest prime factor of 612852475143.
+ * main - find and print the largest prime factor of 612852475143
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	printf("The largest prime factor of 612852475143 is %ld\n",
-			largest_prime_factor(612852475143));
+	printf("%ld\n", largest_prime_factor(612852475143));
 	return (0);
 }
