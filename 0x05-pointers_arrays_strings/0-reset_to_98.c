50c19824af8f830dd8859c4ad096ebba5f37d8f9
Jerdah <idjerop@gmail.com>
2023-09-18 17:48:48 +0000

    A function to set the value pointed by a pointer to 98.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - resets value  pointed to by a pointer to 98.
- * @n: integer 
+ * reset_to_98 - resets value pointed to by a pointer to 98.
+ * @n:pointer to the value
  */
+
 void reset_to_98(int *n)
 {
-    *n = 98; 
+	*n = 98;
 }
