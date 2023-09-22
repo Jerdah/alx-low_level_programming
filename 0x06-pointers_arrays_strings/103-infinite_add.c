478ee09c40f0a9f10f8f7a9b1b7fe5684ee98caa
Jerdah <idjerop@gmail.com>
2023-09-22 23:21:59 +0000

    update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,44 +11,50 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int carry = 0, len1, len2, u, v;
+	int carry = 0, len1, len2, sum;
+	int i = 0;
 
 	for (len1 = 0; n1[len1]; len1++)
-	{
-		for (len2 = 0; n2[len2]; len2++)
-		{
-			if (len1 > size_r || len2 > size_r)
-				return (0);
-		}
-	}
+	{}
+	for (len2 = 0; n2[len2]; len2++)
+	{}
 
 	len1--;
 	len2--;
-	size_r--;
 
-	for (u = 0; u < size_r; u++, len1--, len2--)
+	while (len1 >= 0 || len2 >= 0 || carry)
 	{
+		sum = carry;
 		if (len1 >= 0)
-			carry += n1[len1] - '0';
+		{
+			sum += n1[len1] - '0';
+			len1--;
+		}
 
 		if (len2 >= 0)
-			carry += n2[len2] - '0';
+		{
+			sum += n2[len2] - '0';
+			len2--;
+		}
 
-		r[u] = carry % 10 + '0';
-		carry /= 10;
+		carry = sum / 10;
+		r[i] = (sum % 10) + '0';
+		i++;
+
+		if (i >= size_r - 1)
+			return (0);
 	}
+	r[i] = '\0';
 
-	r[u] = '\0';
-
-	if (len1 >= 0 || len2 >= 0 || carry)
-		return (0);
-
-	for (u--, v = 0; u > v; u--, v++)
-	{
-		carry = r[u];
-		r[u] = r[v];
-		r[v] = carry;
-	}
+	int start = 0, end = i - 1;
+	while (start < end)
+    {
+	    char temp = r[start];
+	    r[start] = r[end];
+	    r[end] = temp;
+	    start++;
+	    end--;
+    }
 
 	return (r);
 }
