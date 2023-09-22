53c4556ed6c498bd4e47b85302eee2bc125a7c6e
Jerdah <idjerop@gmail.com>
2023-09-22 18:09:44 +0000

    a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,10 @@ char *cap_string(char *str)
 	{
 		if (str[u] >= 'a' && str[u] <= 'z')
 		{
-			if (u == 0 || (str[u - 1] >= 9 && str[u - 1] <= 10) || str[u - 1] == 32 || str[u - 1] == 33 || str[u - 1] == 34 || str[u - 1] == 40 || str[u - 1] == 41 || str[u - 1] == 44 || str[u - 1] == 46 || str[u - 1] == 59 || str[u - 1] == 63 || str[u - 1] == 123 || str[u - 1] == 125)
+			if (u == 0 || (str[u - 1] >= 9 && str[u - 1] <= 10) || str[u - 1] == 32 ||
+					str[u - 1] == 33 || str[u - 1] == 34 || str[u - 1] == 40 || str[u - 1] ==
+					41 || str[u - 1] == 44 || str[u - 1] == 46 || str[u - 1] == 59 ||
+					str[u - 1] == 63 || str[u - 1] == 123 || str[u - 1] == 125)
 			{
 				str[u] -= 32;
 			}
