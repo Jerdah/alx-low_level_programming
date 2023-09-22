094449b7cc1e87145dd11e84850ed74a5b27066c
Jerdah <idjerop@gmail.com>
2023-09-22 23:00:23 +0000

    a function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,28 +11,43 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, carry = 0, sum, i;
+	int carry = 0, len1, len2, u, v;
 
-	while (n1[len1])
-		len1++;
-	while (n2[len2])
-		len2++;
+	for (len1 = 0; n1[len1]; len1++)
+	{
+		for (len2 = 0; n2[len2]; len2++)
+		{
+			if (len1 > size_r || len2 > size_r)
+				return (0);
+		}
+	}
 
-	if (len1 + len2 + 1 > size_r)
+	len1--;
+	len2--;
+	size_r--;
+
+	for (u = 0; u < size_r; u++, len1--, len2--)
+	{
+		if (len1 >= 0)
+			carry += n1[len1] - '0';
+
+		if (len2 >= 0)
+			carry += n2[len2] - '0';
+
+		r[u] = carry % 10 + '0';
+		carry /= 10;
+	}
+
+	r[u] = '\0';
+
+	if (len1 >= 0 || len2 >= 0 || carry)
 		return (0);
 
-	r[len1 + len2] = '\0';
-
-	for (i = 1; len1 >= i || len2 >= i || carry; i++)
+	for (u--, v = 0; u > v; u--, v++)
 	{
-		sum = carry;
-		if (len1 >= i)
-			sum += n1[len1 - i] - '0';
-		if (len2 >= i)
-			sum += n2[len2 - i] - '0';
-
-		carry = sum / 10;
-		r[len1 + len2 - i] = sum % 10 + '0';
+		carry = r[u];
+		r[u] = r[v];
+		r[v] = carry;
 	}
 
 	return (r);
