45b68f36fc5866a51b222f9e3aa84c75fdba807c
Jerdah <idjerop@gmail.com>
2023-10-26 01:10:19 +0000

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,6 +10,8 @@
  */
 int _pow_recursion(int x, int y)
 {
+	int output = x;
+
 	if (y < 0)
 	{
 		return (-1);
@@ -18,8 +20,8 @@ int _pow_recursion(int x, int y)
 	{
 		return (1);
 	}
-	else
-	{
-		return (x * _pow_recursion(x, y - 1));
-	}
+
+	output *= _pow_recursion(x, y - 1);
+
+	return (output);
 }
