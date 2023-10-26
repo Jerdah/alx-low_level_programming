6b8666790e2ee2756bc97f53138d25c98a74778d
Jerdah <idjerop@gmail.com>
2023-10-26 12:16:59 +0000

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -51,10 +51,16 @@ int is_palindrome_helper(char *s, int k, int v)
  */
 int is_palindrome(char *s)
 {
-	while (*s && !is_alphanumeric(*s))
+	if (*s == '\0')
 	{
-		s++;
+		return (1);
 	}
-
-	return (is_palindrome_helper(s, 0, strlen(s) - 1));
+	else if (*s != tolower(*(s + strlen(s) - 1)))
+	{
+		return (0);
+	}
+	else
+	{
+		return is_palindrome(s + 1);
+	}
 }
