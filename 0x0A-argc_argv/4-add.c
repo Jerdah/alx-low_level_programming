4d5b0b547d5609ffaa28f9b734b5f5d1602c9438
Jerdah <idjerop@gmail.com>
2023-10-31 17:31:05 +0000

    argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,7 +6,7 @@
 /**
  * main - Entry point
  *
- * @argc: countof command line arguments
+ * @argc: count of command line arguments
  * @argv: array of argument strings
  *
  * Return: 0 (Success),
@@ -14,24 +14,33 @@
  */
 int main(int argc, char *argv[])
 {
-	int v;
+	int i, j, length, sum;
+	char *str;
 
-	for (v = 1; v < argc; v++)
+	if (argc < 2)
+		printf("0\n");
+	else
 	{
-		int i, len = strlen(argv[v]);
-		int sum = 0;
-
-		for (i = 0; i < len; i++)
+		sum = 0;
+		for (i = 1; i < argc; i++)
 		{
-			if (!isdigit(argv[v][i]))
+			str = argv[i];
+			length = strlen(str);
+
+			for (j = 0; j < length; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (isdigit(*(str + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
 
-			sum += atoi(&argv[v][i]);
+			sum += atoi(argv[i]);
 		}
+
 		printf("%d\n", sum);
 	}
+
 	return (0);
 }
