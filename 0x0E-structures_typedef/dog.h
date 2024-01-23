23f847c32ccd4e245ce638ea521d26d199a77768
Jerdah <idjerop@gmail.com>
2024-01-23 19:49:02 +0000

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - elements of a dog
+ * @name: string holding the name of a dog
+ * @age: number representing how old the dog is
+ * @owner: string holding the name of the owner of the dog
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
