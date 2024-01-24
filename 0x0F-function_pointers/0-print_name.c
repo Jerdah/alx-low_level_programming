a02c1b6d444702ca87a0e184db700f539131728d
Jerdah <idjerop@gmail.com>
2024-01-24 10:25:54 +0000

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 
 /**
- * print_name - function prints a name using the function supplied by the caller
+ * print_name - function prints a name using the function
+ * supplied by the caller
  * @name: name to be printed
  * @f: function to call to print the name
  *
