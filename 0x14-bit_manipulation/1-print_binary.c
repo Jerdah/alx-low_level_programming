cf5e9e6044dd28b8f11c73597fc7c3635375aee6
Jerdah <idjerop@gmail.com>
2024-02-08 09:42:35 +0000

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_binary_recursive - helper function to print the binary representation
+ * _print_binary - helper function to print the binary representation
  * @n: number to print in binary
  */
-void print_binary_recursive(unsigned long int n)
+void _print_binary(unsigned long int n)
 {
 	if (n > 1)
-		print_binary_recursive(n >> 1);
+		_print_binary(n >> 1);
 
 	_putchar((n & 1) + '0');
 }
@@ -21,7 +21,7 @@ void print_binary(unsigned long int n)
 	if (n == 0)
 		_putchar('0');
 	else
-		print_binary_recursive(n);
+		_print_binary(n);
 
 	_putchar('\n');
 }
