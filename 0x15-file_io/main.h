559e9d227304864bfba6272fd3d391e4ada2b851
Jerdah <idjerop@gmail.com>
2024-02-12 17:55:36 +0000

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,9 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_error(int code, const char *file, int ac, char **av);
-int main(int ac, char **av);
+int _close(int fd);
+ssize_t _read(const char *filename, int fd, char *buf, size_t count);
+ssize_t _write(const char *filename, int fd, const char *buf, size_t count);
+void print_error(int code, const char *file);
 
 #endif
