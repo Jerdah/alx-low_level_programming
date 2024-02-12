8d6c9c8cf1d5e66b79490e8e9ec944fced4ad63f
Jerdah <idjerop@gmail.com>
2024-02-12 18:02:55 +0000

    updates

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -60,37 +60,12 @@ ssize_t _write(const char *filename, int fd, const char *buf, size_t count)
 }
 
 /**
- * print_error - Print error message with proper format
- * @code: The error code
- * @file: The filename associated with the error
- */
-void print_error(int code, const char *file)
-{
-	switch (code)
-	{
-		case 97:
-			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-			break;
-		case 98:
-			dprintf(STDERR_FILENO, "Error: Cannot read from file %s\n", file);
-			break;
-		case 99:
-			dprintf(STDERR_FILENO, "Error: Cannot write to %s\n", file);
-			break;
-		case 100:
-			dprintf(STDERR_FILENO, "Error: Cannot close fd\n");
-			break;
-		default:
-			break;
-	}
-}
-
-/**
  * main - copies the contents of one file to another
  * @argc: argument count
  * @argv: argument values
  *
  * Return: Always 1
+ *
  */
 int main(int argc, const char *argv[])
 {
@@ -100,25 +75,22 @@ int main(int argc, const char *argv[])
 
 	if (argc != 3)
 	{
-		print_error(97, NULL);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_in = open(argv[1], O_RDONLY);
 	if (fd_in < 0)
 	{
-		print_error(98, argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_out = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_out < 0)
 	{
-		print_error(99, argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		_close(fd_in);
 		exit(99);
 	}
-
 	while ((bytes_read = _read(argv[1], fd_in, buffer, BUFSIZE)))
 	{
 		if (bytes_read < 0)
@@ -127,7 +99,6 @@ int main(int argc, const char *argv[])
 			_close(fd_out);
 			exit(98);
 		}
-
 		if (_write(argv[2], fd_out, buffer, bytes_read) < 0)
 		{
 			_close(fd_in);
@@ -135,9 +106,7 @@ int main(int argc, const char *argv[])
 			exit(99);
 		}
 	}
-
 	if ((_close(fd_in) | _close(fd_out)) < 0)
 		exit(100);
-
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,6 +12,5 @@ int append_text_to_file(const char *filename, char *text_content);
 int _close(int fd);
 ssize_t _read(const char *filename, int fd, char *buf, size_t count);
 ssize_t _write(const char *filename, int fd, const char *buf, size_t count);
-void print_error(int code, const char *file);
 
 #endif
