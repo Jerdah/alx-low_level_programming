85cc5ae8f5e7a94a97aff61e801f044dcfc723e6
Jerdah <idjerop@gmail.com>
2024-05-30 01:34:04 +0200

    update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,20 +2,17 @@
 """
 Island Perimeter module
 """
-
 def island_perimeter(grid):
-    """
-    Compute the perimeter of an island
-    """
+    """Computes the perimeter of an island"""
     perimeter = 0
     for i, row in enumerate(grid):
-        for j, cell in enumerate(row):
-            if cell == 1:
+        for j, item in enumerate(row):
+            if item == 1:
                 if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
                 if j == len(row) - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
-                if i == 0 or grid[i - 1][j] == 0:  # Check above
+                if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
                 if i == len(grid) - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
